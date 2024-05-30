287f061cbda1c798bc1f3bb8102c7596cada6440
Essential Randomness <essential.randomn3ss@gmail.com>
2024-05-30 12:38:02 -0700

    update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Boba test";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   
     deploy-rs = {
