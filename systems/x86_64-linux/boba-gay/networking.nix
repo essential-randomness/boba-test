1d54a10d69a2a66f0b3d1738d67796f336e199c9
Essential Randomness <essential.randomn3ss@gmail.com>
2024-06-02 15:35:41 -0700

    new machine

--- systems/x86_64-linux/boba-gay/networking.nix
+++ systems/x86_64-linux/boba-gay/networking.nix
@@ -3,9 +3,9 @@
   # details gathered from the active system.
   networking = {
     nameservers = [ "8.8.8.8" ];
-    defaultGateway = "138.197.80.1";
+    defaultGateway = "172.31.1.1";
     defaultGateway6 = {
-      address = "";
+      address = "fe80::1";
       interface = "eth0";
     };
     dhcpcd.enable = false;
@@ -13,27 +13,20 @@
     interfaces = {
       eth0 = {
         ipv4.addresses = [
-          { address="138.197.82.230"; prefixLength=20; }
-          { address="10.17.0.7"; prefixLength=16; }
+          { address="5.161.76.29"; prefixLength=32; }
         ];
         ipv6.addresses = [
-          { address="fe80::8470:43ff:fe6d:a1ae"; prefixLength=64; }
+          { address="2a01:4ff:f0:c739::1"; prefixLength=64; }
+          { address="fe80::9400:3ff:fe5f:689f"; prefixLength=64; }
         ];
-        ipv4.routes = [ { address = "138.197.80.1"; prefixLength = 32; } ];
-        ipv6.routes = [ { address = ""; prefixLength = 128; } ];
+        ipv4.routes = [ { address = "172.31.1.1"; prefixLength = 32; } ];
+        ipv6.routes = [ { address = "fe80::1"; prefixLength = 128; } ];
       };
-      eth1 = {
-        ipv4.addresses = [
-          { address="10.108.0.5"; prefixLength=20; }
-        ];
-        ipv6.addresses = [
-          { address="fe80::9090:adff:fec0:c193"; prefixLength=64; }
-        ];
-      };
+
     };
   };
   services.udev.extraRules = ''
-    ATTR{address}=="86:70:43:6d:a1:ae", NAME="eth0"
-    ATTR{address}=="92:90:ad:c0:c1:93", NAME="eth1"
+    ATTR{address}=="96:00:03:5f:68:9f", NAME="eth0"
+
   '';
 }
