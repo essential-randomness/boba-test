812aff7f8c5422e6adc1930545618e8ca01a0b11
Essential Randomness <essential.randomn3ss@gmail.com>
2024-06-03 22:23:12 -0700

    try import

--- systems/x86_64-linux/boba-gay/default.nix
+++ systems/x86_64-linux/boba-gay/default.nix
@@ -4,6 +4,7 @@
     # Generated at runtime by nixos-infect
     ./networking.nix
     inputs.vscode-server.nixosModules.default
+    inputs.bobaboard.nixosModules.bobaboard
   ];
 
   services.openssh.enable = true;
